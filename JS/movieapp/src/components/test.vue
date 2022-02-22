6cbe4cdf59e8380bdf4a825e61c6823886db644b
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-02-22 14:13:56 -0600

    I'm having issue using the await function to work with test api

---
+++ JS/movieapp/src/components/test.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>MAIN VIEW TEST</h1>
+    <section>
+        <input type="text" 
+        placeholder="Insert Movie Name"
+        :value="movie"/>
+        <button
+        v-on:click= getdata()>Submit</button>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'test',
+    props: []
+}
+getdata()
+async function getdata(){
+    const response = await fetch('https://ghibliapi.herokuapp.com/films')
+    const data = await response.json()
+    } //Trying to test use of APIs with the Ghibli API but it isn't working...
+
+</script>
