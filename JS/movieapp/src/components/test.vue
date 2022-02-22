d4fb2ad24e13e27349e3a1cde83d35e020361e7b
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-02-22 17:04:50 -0600

    Update test.vue

--- JS/movieapp/src/components/test.vue
+++ JS/movieapp/src/components/test.vue
@@ -7,6 +7,10 @@ getdata()//API console.log works now but it logs on load instead of on the click
 async function getdata(){
     const response = await fetch('https://ghibliapi.herokuapp.com/films')
     const data = await response.json()
+    const obj = JSON.parse(this.data)
+    obj.forEach(movie=> {
+        console.log(movie.title)
+    });
     console.log(data)
     } 
 </script>
@@ -15,8 +19,7 @@ async function getdata(){
     <h1>MAIN VIEW TEST</h1>
     <section>
         <input type="text" 
-        placeholder="Insert Movie Name"
-        :value="movie"/>
+        placeholder="Insert Movie Name"/>
         <button
         @click= "getdata()">Submit</button>
     </section>
