6674f147c349d9ec915ddae587a98700502c094f
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-03-15 14:53:08 -0500

    Update test.vue
    Having issues with getting an API for Distance Matrix may have to rework application.

--- JS/movieapp/src/components/test.vue
+++ JS/movieapp/src/components/test.vue
@@ -1,11 +1,11 @@
 <template>
     <h1>MAIN VIEW TEST</h1>
     <section id = "#Runner">
-        <input id="Input1"
+        <input id="Orig"
         type="text" 
-        placeholder="Doesn't do anything"/>
-        <input id="Input2" type="text"
-        placeholder="Neither does this one"/>
+        placeholder="From"/>
+        <input id="Dest" type="text"
+        placeholder="To"/>
         <button
         v-on:click= "getmovie()">Submit</button>
         <div id="movie">{{name}}</div>
@@ -38,7 +38,14 @@ export default {
             results.forEach(movie => {
                 console.log(movie.title)
             })
-        }
+        },
+        async getdistance(){
+            var from_value = from_value
+            document.getElementById('Orig').value = from_value
+            var to_value = to_value
+            document.getElementById('Dest').value = to_value
+            const dist_response = await fetch(`https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${from_value}&destinations=${to_value}=<your_access_token>`) //Distance Matrix API Set to expire on 3/18/21 currently looking for a new one
+        } //This function gets the distance between 2 points.
     },
 }
 
