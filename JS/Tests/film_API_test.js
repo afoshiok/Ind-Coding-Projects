c5a850048feaa80562cba3405ae7c1aa2399e445
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-05-14 19:42:15 -0500

    Update film_API_test.js

--- JS/Tests/film_API_test.js
+++ JS/Tests/film_API_test.js
@@ -1,8 +1,8 @@
 require('dotenv').config({path: '.env'}); //Imports dotenv
 
 const tmdb_key = process.env.TMDB_KEY
-const query_runtime = `https://api.themoviedb.org/3/discover/movie?api_key=${tmdb_key}&language=en-US&sort_by=popularity.desc&vote_average.gte=8&with_runtime.lte=45` //Queried by average vote and runtime BUT runtime is not working...
-const film_info = `https://api.themoviedb.org/3/movie/747059?api_key=${tmdb_key}&language=en-US`
+const query_runtime = `https://api.themoviedb.org/3/discover/movie?api_key=${tmdb_key}&language=en-US&sort_by=popularity.desc&vote_average.gte=8&with_runtime.lte=45` //Queried by average vote and runtime [Runtime works now]
+const film_info = `https://api.themoviedb.org/3/movie/831827?api_key=${tmdb_key}&language=en-US`
 
 
 fetch(query_runtime)
@@ -12,10 +12,10 @@ fetch(query_runtime)
     console.log(result)
 });
 
-// fetch(film_info)
-// .then(response => response.json())
-// .then(data => {
-//     var result = data
-//     console.log(data)
-// });
+fetch(film_info)
+.then(response => response.json())
+.then(data => {
+    var result = data
+    console.log(result)
+});
 
