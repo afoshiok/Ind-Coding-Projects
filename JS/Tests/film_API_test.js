3453d666e61127dc4d15c7a1f464c77c0291d589
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-05-14 20:07:37 -0500

    Update film_API_test.js

--- JS/Tests/film_API_test.js
+++ JS/Tests/film_API_test.js
@@ -5,17 +5,30 @@ const query_runtime = `https://api.themoviedb.org/3/discover/movie?api_key=${tmd
 const film_info = `https://api.themoviedb.org/3/movie/831827?api_key=${tmdb_key}&language=en-US`
 
 
-fetch(query_runtime)
-.then(response => response.json())
-.then(data => {
+
+function get_movies() {
+    fetch(query_runtime)
+    .then(response => response.json())
+    .then(data => {
     var result = data.results[0]
     console.log(result)
-});
+})    
+}
+
+let runtime_filter = 400  //this will be later replaced with the flight duration
 
 fetch(film_info)
 .then(response => response.json())
 .then(data => {
     var result = data
     console.log(result)
+    runtime_filter = runtime_filter - result.runtime //This subracts the movie runtime from the runtime filter. This will be done over and over from the loop
+    console.log(runtime_filter)
 });
+
+
+
+// while (runtime_filter > 10 ){
+//     get_movies()
+// }
 
