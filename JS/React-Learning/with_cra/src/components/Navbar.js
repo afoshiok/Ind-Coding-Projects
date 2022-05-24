3fe64ae7cf6c1496364055a5e59e572306c90934
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-05-24 15:02:27 -0500

    Working with React Components

---
+++ JS/React-Learning/with_cra/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+export default class Navbar extends Component{
+    render(){
+        return(
+        <nav>
+            <h1>Navbar</h1>
+            <ul>
+                {/* <h1>Thing 1</h1> Doesn't display */}
+            </ul>
+        </nav>
+        ) 
+    }
+}
