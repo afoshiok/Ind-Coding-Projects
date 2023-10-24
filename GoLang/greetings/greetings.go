a9819ed36174c6ca9d815f62cc771744b8310897
afoshiok <89757138+afoshiok@users.noreply.github.com>
2023-10-24 15:58:13 -0500

    Returning maps

--- GoLang/greetings/greetings.go
+++ GoLang/greetings/greetings.go
@@ -22,7 +22,7 @@ func Hello(name string) (string, error) { //function pattern: func Name(var_Name
 
 func randomFormat() string {
 	//A slice of message format
-	formats := []string{
+	formats := []string{ //This is known as a "slice"
 		"Hi, %v. Welcome!",
 		"Great to see you, %v!",
 		"Hail, %v! Well met!",
@@ -30,4 +30,20 @@ func randomFormat() string {
 
 	//Return a randomly selected message by specifying a random index for the slice of formats.
 	return formats[rand.Intn(len(formats))]
+}
+
+//Hellos returns a map that associates each of the named people with a greeting message
+func Hellos(names []string) (map[string]string, error) { //In Go you initialize a map with map[<key type>]<value type>
+	//A map to associate name with messages.
+	messages := make(map[string]string)
+	//Loop through the recieved slice of names, calling the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		//In the map, associate the retrieved message with the name
+		messages[name] = message
+	}
+	return messages, nil
 }
