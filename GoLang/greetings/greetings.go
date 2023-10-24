d47d0bbcadfaa05616753ea25ee9e0ac549002cd
afoshiok <89757138+afoshiok@users.noreply.github.com>
2023-10-24 15:04:50 -0500

    Working with functions...in Go

--- GoLang/greetings/greetings.go
+++ GoLang/greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 //Hello returns a greeting for the named person.
@@ -14,7 +15,19 @@ func Hello(name string) (string, error) { //function pattern: func Name(var_Name
 
 	//If a name was recieved,
 	//Return a greeting that embeds the name in a message.
-	var message string
-	message = fmt.Sprintf("Hello my name is %v", name)
+	var message string // or you can use := for implied type
+	message = fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	//A slice of message format
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	//Return a randomly selected message by specifying a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
